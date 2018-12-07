52b667e900bb947cc84e932c3eb9594203ad1c58
ebenfrk <fredrik.berglin@ericsson.com>
2018-12-07 06:08:29 +0100

    Day 3

---
+++ 3b.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+
+open($fh, "< 3.input") || die "Unable to open file: $!\n";
+
+@input = <$fh>;
+
+<<EOC
+@input = ( "#1 @ 1,3: 4x4",
+           "#2 @ 3,1: 4x4",
+           "#3 @ 5,5: 2x2" );
+EOC
+;
+
+%grid = ();
+
+foreach $claim (@input) {
+    chomp($claim);
+    if (not $claim =~ m/#\d+ @ (\d+),(\d+): (\d+)x(\d+)/) {
+        print "Unknown pattern: '$claim'. Exiting.\n";
+        exit;
+    }
+
+    $xStart = $1;
+    $yStart = $2;
+    $xSize = $3;
+    $ySize = $4;
+
+    foreach $x ($xStart ... $xStart + $xSize - 1) {
+        foreach $y ($yStart ... $yStart + $ySize - 1) {
+            if (defined($grid{"$x,$y"})) {
+                $grid{"$x,$y"}++;
+            } else {
+                $grid{"$x,$y"} = 1;
+            }
+        }
+    }
+}
+
+foreach $claim (@input) {
+    chomp($claim);
+    if (not $claim =~ m/#\d+ @ (\d+),(\d+): (\d+)x(\d+)/) {
+        print "Unknown pattern: '$claim'. Exiting.\n";
+        exit;
+    }
+
+    $xStart = $1;
+    $yStart = $2;
+    $xSize = $3;
+    $ySize = $4;
+
+    $singleClaim = 1;
+    foreach $x ($xStart ... $xStart + $xSize - 1) {
+        foreach $y ($yStart ... $yStart + $ySize - 1) {
+            if ($grid{"$x,$y"} > 1) {
+                $singleClaim = 0;
+                last;
+            }
+            if ($singleClaim == 0) {
+                last;
+            }
+        }
+    }
+    if ($singleClaim == 1) {
+        print "Unshared claim: '$claim'\n";
+    }
+}
+
+
